4cb1638e860c2dfbbb7bdbfe1819305f9439195b
sudharsanam007-design <sudharsanam007@gmail.com>
2026-02-07 19:44:17 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,35 @@
 
+import joblib
+import pandas as pd
+
+def main():
+    model = joblib.load("models/best_model.joblib")
+
+    sample = {
+        "Age": 35,
+        "TypeofContact": "Self Inquiry",
+        "CityTier": 1,
+        "Occupation": "Salaried",
+        "Gender": "Male",
+        "NumberOfPersonVisiting": 2,
+        "PreferredPropertyStar": 4,
+        "MaritalStatus": "Married",
+        "NumberOfTrips": 3,
+        "Passport": 1,
+        "OwnCar": 1,
+        "NumberOfChildrenVisiting": 0,
+        "Designation": "Manager",
+        "MonthlyIncome": 60000,
+        "PitchSatisfactionScore": 4,
+        "ProductPitched": "Wellness",
+        "NumberOfFollowups": 2,
+        "DurationOfPitch": 20
+    }
+
+    df = pd.DataFrame([sample])
+    pred = model.predict(df)[0]
+
+    print("Prediction successful:", pred)
+
+if __name__ == "__main__":
+    main()
