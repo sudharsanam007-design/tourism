feb28ecca2f2924c1176fa4b2b3b699c923a0568
sudharsanam007-design <sudharsanam007@gmail.com>
2026-02-07 19:43:57 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,25 @@
 
+from datasets import load_dataset
+import pandas as pd
+import joblib
+from sklearn.metrics import accuracy_score, roc_auc_score
+
+HF_DATASET = "Sudharsanamr/tourism"
+
+def main():
+    ds = load_dataset(HF_DATASET)
+    test_df = ds["test"].to_pandas()
+
+    X_test = test_df.drop("ProdTaken", axis=1)
+    y_test = test_df["ProdTaken"]
+
+    model = joblib.load("models/best_model.joblib")
+
+    preds = model.predict(X_test)
+    probs = model.predict_proba(X_test)[:, 1]
+
+    print("Accuracy :", accuracy_score(y_test, preds))
+    print("ROC-AUC  :", roc_auc_score(y_test, probs))
+
+if __name__ == "__main__":
+    main()
